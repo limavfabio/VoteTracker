746df5a860c8df068e6d61ca27e35cf486d13780
Fábio Lima <limavfabio@gmail.com>
2024-02-25 19:04:32 -0300

    Cleanup on VotesController

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,35 +1,24 @@
 class VotesController < ApplicationController
-  before_action :set_vote, only: %i[ show edit update destroy ]
+  before_action :set_vote, only: %i[update destroy]
 
-  # GET /votes or /votes.json
-  def index
-    @votes = Vote.all
-  end
+  # GET /votes/1/edit
+  def edit; end
 
-  # GET /votes/1 or /votes/1.json
-  def show
-  end
-
-  # GET /votes/new
   def new
     @vote = Vote.new
   end
 
-  # GET /votes/1/edit
-  def edit
-  end
-
   # POST /votes or /votes.json
   def create
-    @vote = Vote.new(vote_params)
+    @vote = current_user.votes.build(vote_params)
 
     respond_to do |format|
       if @vote.save
-        format.html { redirect_to vote_url(@vote), notice: "Vote was successfully created." }
+        format.html { redirect_to root_path, notice: 'Vote was successfully created.' }
         format.json { render :show, status: :created, location: @vote }
       else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @vote.errors, status: :unprocessable_entity }
+        flash[:alert] = @vote.errors.full_messages.join(', ')
+
       end
     end
   end
@@ -37,8 +26,8 @@ class VotesController < ApplicationController
   # PATCH/PUT /votes/1 or /votes/1.json
   def update
     respond_to do |format|
-      if @vote.update(vote_params)
-        format.html { redirect_to vote_url(@vote), notice: "Vote was successfully updated." }
+      if @vote.update(vote_params, user: current_user)
+        format.html { redirect_to vote_url(@vote), notice: 'Vote was successfully updated.' }
         format.json { render :show, status: :ok, location: @vote }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -52,19 +41,20 @@ class VotesController < ApplicationController
     @vote.destroy!
 
     respond_to do |format|
-      format.html { redirect_to votes_url, notice: "Vote was successfully destroyed." }
+      format.html { redirect_to votes_url, notice: 'Vote was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_vote
-      @vote = Vote.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def vote_params
-      params.require(:vote).permit(:candidate_id)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_vote
+    @vote = Vote.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def vote_params
+    params.require(:vote).permit(:candidate_id)
+  end
 end
