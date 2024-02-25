4301c0de184ad6178e77097b3ef4583157953e67
Fábio Lima <limavfabio@gmail.com>
2024-02-25 19:04:11 -0300

    Prevent seeds from creating votes

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,19 +6,20 @@ party3 = Party.create(name: 'Party C')
 
 8.times do
   party = [party1, party2, party3].sample
-  office = ['Governor', 'Mayor', 'Councilor'].sample
-  Candidate.create(name: Faker::Name.unique.name, party: party, office: office)
+  office = %w[Governor Mayor Councilor].sample
+  Candidate.create(name: Faker::Name.unique.name, party:, office:)
 end
 
 User.create(email: 'john@example.com', password: 'password', password_confirmation: 'password', verified: true)
 
 25.times do
-  User.create(email: Faker::Internet.unique.email, password: 'password', password_confirmation: 'password', verified: true)
+  User.create(email: Faker::Internet.unique.email, password: 'password', password_confirmation: 'password',
+              verified: true)
 end
 
-50.times do
-  candidate = Candidate.all.sample
-  user = User.all.sample
-  vote = Vote.new(candidate: candidate, user: user)
-  vote.save! if vote.valid?
-end
+# 50.times do
+#   candidate = Candidate.all.sample
+#   user = User.all.sample
+#   vote = Vote.new(candidate:, user:)
+#   vote.save! if vote.valid?
+# end
